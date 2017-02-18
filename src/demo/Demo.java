a1edd1accacd4a7ce7b5efe0e120c86afd54d8d3
Kai <clarke157@aliyun.com>
2017-02-18 15:24:48 +0800

    3:24 pm

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("How are you?"
-				+ "3:23pm updated");
+				+ "3:24pm updated");
 
 	}
 
