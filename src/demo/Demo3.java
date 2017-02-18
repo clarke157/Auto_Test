4c8aee91fd9cc2d30ca1a4328e163f27f04b9c90
Kai <clarke157@aliyun.com>
2017-02-18 15:57:45 +0800

    modified 3:56pm

--- src/demo/Demo3.java
+++ src/demo/Demo3.java
@@ -5,6 +5,7 @@ public class Demo3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is demo3-updated on 3:20pm");
+		System.out.println("This is demo3-updated on 3:56pm");
 
 	}
 
