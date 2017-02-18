9abc7d6ac812df62575ebabfb88ef6f4557a3788
Kai <clarke157@aliyun.com>
2017-02-18 15:21:31 +0800

    modified on 3:20pm 20170218

---
+++ src/demo/Demo3.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is demo3-updated on 3:20pm");
+
+	}
+
+}
